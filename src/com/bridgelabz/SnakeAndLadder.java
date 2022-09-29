2461fff2a67235e60bfb868f41e0fd8cc1d16846
Romil003 <romilghadge33@gmail.com>
2022-09-30 00:10:39 +0530

    Added UC6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -6,15 +6,21 @@ public class SnakeAndLadder {
     static int player1 = 0;
     public static void main(String[] args) {
         int dieRoll = 0;
+        int numDieRoll = 0;
 
         while(player1 != 100){
             dieRoll = (int)Math.floor(Math.random()*6)+1;
+            numDieRoll++;
+            System.out.println("Number of times die rolls is " + numDieRoll);
             System.out.println("Die roll value = " + dieRoll);
             player1 = dieRoll + player1;
             if (player1 >100 ){
                 player1 = player1 - dieRoll;
             }
             System.out.println(player1);
+            if(player1 == 100 ){
+                break;
+            }
             int option = (int)Math.floor(Math.random()*3);
             switch (option) {
                 case 0 :
