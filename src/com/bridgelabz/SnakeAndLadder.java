20b31b2af4d8b862cd57bad64abfbb7895e5c085
Romil003 <romilghadge33@gmail.com>
2022-09-29 21:15:38 +0530

    Added UC1

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+    static int player1 = 0;
+    public static void main(String[] args) {
+        for(int i = 0 ; i <= 100 ; i++){
+            player1 = i;
+            System.out.println(player1);
+        }
+
+    }
+}
