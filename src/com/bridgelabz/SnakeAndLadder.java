b5ddda1f4a37eff6404012e847044cf99a654299
Romil003 <romilghadge33@gmail.com>
2022-09-29 22:10:50 +0530

    Added UC2

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,12 +1,23 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     static int player1 = 0;
     public static void main(String[] args) {
-        for(int i = 0 ; i <= 100 ; i++){
-            player1 = i;
+        int dieRoll = 0;
+
+        while(player1 != 100){
+            dieRoll = (int)Math.floor(Math.random()*6)+1;
+            System.out.println("Die roll value = " + dieRoll);
+            player1 = dieRoll + player1;
+            if (player1 >100 ){
+                player1 = player1 - dieRoll;
+            }
             System.out.println(player1);
         }
+
+
 
     }
 }
