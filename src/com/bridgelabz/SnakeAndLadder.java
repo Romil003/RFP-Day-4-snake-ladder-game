731f6f81af6b9fee9b58a0dc677b0c405ac5fa2a
Romil003 <romilghadge33@gmail.com>
2022-09-30 17:38:31 +0530

    Added UC7

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,43 +4,64 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     static int player1 = 0;
+    static int player2 = 0;
     public static void main(String[] args) {
-        int dieRoll = 0;
-        int numDieRoll = 0;
+        int dieRoll1 = 0;
+        int dieRoll2 = 0;
+        int numDieRoll1 = 0;
+        int numDieRoll2 = 0;
 
-        while(player1 != 100){
-            dieRoll = (int)Math.floor(Math.random()*6)+1;
-            numDieRoll++;
-            System.out.println("Number of times die rolls is " + numDieRoll);
-            System.out.println("Die roll value = " + dieRoll);
-            player1 = dieRoll + player1;
-            if (player1 >100 ){
-                player1 = player1 - dieRoll;
+        while(player1 != 100 && player2 != 100) {
+            dieRoll1 = (int) Math.floor(Math.random() * 6) + 1;
+            numDieRoll1++;
+            System.out.println("Number of times die rolls for Player1 is " + numDieRoll1);
+            System.out.println("For Player1 Die roll value = " + dieRoll1);
+            player1 = dieRoll1 + player1;
+            dieRoll2 = (int) Math.floor(Math.random() * 6) + 1;
+            numDieRoll2++;
+            System.out.println("Number of times die rolls for Player2 is " + numDieRoll2);
+            System.out.println("For Player2 Die roll value = " + dieRoll2);
+            player2 = dieRoll2 + player2;
+            if (player1 > 100) {
+                player1 = player1 - dieRoll1;
             }
-            System.out.println(player1);
-            if(player1 == 100 ){
+            if (player2 > 100) {
+                player2 = player2 - dieRoll2;
+            }
+            System.out.println("Player1 is " + player1);
+            System.out.println("Player2 is " + player2);
+            if (player1 == 100 || player2 == 100) {
                 break;
             }
-            int option = (int)Math.floor(Math.random()*3);
-            switch (option) {
-                case 0 :
-                    System.out.println("No play,remain at position ...");
-                    break;
-                case 1 :
+            if (player1 == 11 || player1 == 24 || player1 == 39 || player1 == 48 || player1 == 60 || player1 == 70 || player1 == 79 || player1 == 85) {
+                System.out.println("Whoow !! Player1 Got ladder ...");
+                dieRoll1 = (int) Math.floor(Math.random() * 6) + 1;
+                player1 += dieRoll1;
+                System.out.println("Player2 stops ");
+                System.out.println(player1);
+            } else if (player1 == 17 || player1 == 30 || player1 == 38 || player1 == 54 || player1 == 69 || player1 == 78 || player1 == 83 || player1 == 95) {
+                System.out.println("Oops !!! Player1 beaten by snake buddy ...");
+                player1 -= dieRoll1;
+                System.out.println(player1);
+            } else {
+                System.out.println("Player1 is at" + player1);
+            }
 
-                    if(player1<80) {
-                        System.out.println("Whoow !! Got ladder ...");
-                        player1 += dieRoll;
-                    }
-                    System.out.println(player1);
-                    break;
-                case 2:
-                    if(player1>18 && player1<95) {
-                        System.out.println("Oops !!! beaten by snake buddy ...");
-                        player1 -= dieRoll;
-                    }
-                    System.out.println(player1);
-                    break;
+            if(player2 == 11 || player2 == 24 || player2 == 39 || player2 == 48 || player2 == 60 || player2 == 70 || player2 == 79 || player2 == 85) {
+                System.out.println("Whoow !! Player2 Got ladder ...");
+                dieRoll2 = (int) Math.floor(Math.random() * 6) + 1;
+                player2 += dieRoll2;
+                System.out.println("Player1 stops ...");
+                System.out.println(player2);
+            }
+            else if(player1 == 17 || player1 == 30 || player1 == 38 || player1 == 54 || player1 == 69 || player1 == 78 || player1 == 83 || player1 == 95) {
+                System.out.println("Oops !!! Player2 beaten by snake buddy ...");
+                player2 -= dieRoll2;
+                System.out.println(player2);
+            } else {
+                System.out.println("Player2 is at " + player2);
+            }
+
 
             }
         }
@@ -48,4 +69,4 @@ public class SnakeAndLadder {
 
 
     }
-}
+
